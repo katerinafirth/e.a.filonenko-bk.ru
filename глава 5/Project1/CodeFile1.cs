9175576b725a312e9b5e3baf6a1e7e4fae6762d5
katerinafirth <58289298+katerinafirth@users.noreply.github.com>
2020-03-31 17:47:48 +0500

    Add files via upload

---
+++ глава 5/Project1/CodeFile1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+namespace Petzold.DesignAButton
+{
+    public class DesignAButton : Window
+    {
+        [STAThread] public static void Main()
+        {
+            Application app = new Application(); // инициализация объекта 
+            app.Run(new DesignAButton());
+        }
+        public DesignAButton()
+        {
+            Title = "Design a Button"; // создание кнопки как содержимое окна 
+            Button btn = new Button(); // инициализация кнопки 
+            btn.HorizontalAlignment = HorizontalAlignment.Center; // центрирование + строчка ниже 
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.Click += ButtonOnClick; // происходит действие при нажатии кнопки 
+            Content = btn; // Create a StackPanel as content of the Button.
+            StackPanel stack = new StackPanel(); // инициализация нового экземпляра класса (выравнивание)
+            btn.Content = stack; // Add a Polyline to the StackPanel.
+            stack.Children.Add(ZigZag(10)); // Add an Image to the StackPanel.
+            Uri uri = new Uri("pack://application: ,,/BOOK06.ICO"); // 32-pixels
+            BitmapImage bitmap = new BitmapImage(); // инициализация нового экземпляра класса (объект для загрузки изображений)
+            Image img = new Image(); // инициализация нового экземпляра класса (изображение)
+            img.Margin = new Thickness(0, 10, 0, 0); // инициализация нового экземпляра структуры (описание поля) + 2 строчки ниже 
+            img.Source = bitmap;
+            img.Stretch = Stretch.None; // описание способа изменения размеров 
+            stack.Children.Add(img); // Add a Label to the StackPanel.
+            Label lbl = new Label(); // инициализация нового экземпляра класса (текст)
+            lbl.Content = "_Read books!";
+            lbl.HorizontalContentAlignment = HorizontalAlignment.Center; // выравнивание по горизонтали 
+            stack.Children.Add(lbl); // Add another Polyline to the StackPanel.
+            stack.Children.Add(ZigZag(0));
+        }
+        Polyline ZigZag(int offset) // рисование 
+        {
+            Polyline poly = new Polyline();
+            poly.Stroke = SystemColors.ControlTextBrush;
+            poly.Points = new PointCollection();
+            for (int x = 0; x <= 100; x += 10)
+                poly.Points.Add(new Point(x, (x + offset) % 20));
+            return poly;
+        }
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            MessageBox.Show("The button has been clicked", Title);
+        }
+    }
+}
