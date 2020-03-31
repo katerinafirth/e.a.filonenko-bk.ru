b143d94a5dec7b21bf63f22d727664cd3eb11839
katerinafirth <58289298+katerinafirth@users.noreply.github.com>
2020-03-31 18:31:16 +0500

    Add files via upload

---
+++ глава 2/глава 2/CodeFile1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+namespace Petzold.AdjustTheGradient
+{
+    class AdjustTheGradient : Window
+    {
+        LinearGradientBrush brush;[STAThread]
+        public static void Main()
+        {
+            Application app = new Application(); // инициализация объекта
+            app.Run(new AdjustTheGradient());
+        }
+        public AdjustTheGradient()
+        {
+            Title = "Adjust the Gradient"; //заголовок
+            SizeChanged += WindowOnSizeChanged;
+            brush = new LinearGradientBrush(Colors.Red, Colors.Blue, 0); //выбор цветов
+            brush.MappingMode = BrushMappingMode.Absolute;
+            Background = brush;
+        }
+        void WindowOnSizeChanged(object sender, SizeChangedEventArgs args)
+        { //функция , объявление объектов
+            double width = ActualWidth - 2 * SystemParameters.ResizeFrameVerticalBorderWidth; //формат объектов, и строчка ниже, + условия выполнения
+            double height = ActualHeight - 2 * SystemParameters.ResizeFrameHorizontalBorderHeight - SystemParameters.CaptionHeight;
+            Point ptCenter = new Point(width / 2, height / 2);
+            Vector vectDiag = new Vector(width, -height); //параметры закраски, строчки выше и ниже
+            Vector vectPerp = new Vector(vectDiag.Y, -vectDiag.X);
+            vectPerp.Normalize(); vectPerp *= width * height / vectDiag.Length;
+            brush.StartPoint = ptCenter + vectPerp; brush.EndPoint = ptCenter - vectPerp;
+        }
+    }
+}
