5b7cb21c43824b7ccfbde0cda6013362f9b5db8c
katerinafirth <58289298+katerinafirth@users.noreply.github.com>
2020-03-31 17:30:15 +0500

    Add files via upload

---
+++ глава 4/Project1/CodeFile1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+namespace Petzold.CommandTheButton {
+    public class CommandTheButton : Window { [STAThread]
+        public static void Main() {
+            Application app = new Application(); // инициализация
+            app.Run(new CommandTheButton());
+        }
+        public CommandTheButton(){ // объявление функции 
+            Title = "Command the Button";             // Create the Button and set as window  content - название             
+            Button btn = new Button(); // инициализация кнопки
+            btn.HorizontalAlignment =  HorizontalAlignment.Center; // центрирование + строчка ниже
+            btn.VerticalAlignment =  VerticalAlignment.Center;
+            btn.Command = ApplicationCommands.Paste;
+            btn.Content = ApplicationCommands .Paste.Text;
+            Content = btn;             // Bind the command to the event handlers. - связывание команд с обработчиками событий      
+            CommandBindings.Add(new CommandBinding (ApplicationCommands.Paste, PasteOnExecute, PasteCanExecute)); // инициализация 
+        }
+        void PasteOnExecute(object sender,  ExecutedRoutedEventArgs args)
+        {Title = Clipboard.GetText(); // отправляем данные заголовка в буфер обмена
+        }
+        void PasteCanExecute(object sender,  CanExecuteRoutedEventArgs args)
+        {args.CanExecute = Clipboard .ContainsText();
+        }
+        protected override void OnMouseDown (MouseButtonEventArgs args) // предоставляет данные для кнопки мыши
+        {base.OnMouseDown(args);
+            Title = "Command the Button";
+        }
+    }
+} 
