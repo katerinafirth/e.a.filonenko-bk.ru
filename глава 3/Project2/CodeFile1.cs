933e195dc0ff13316b87e7436d3876706adf12c3
katerinafirth <58289298+katerinafirth@users.noreply.github.com>
2020-03-31 16:55:00 +0500

    Add files via upload

---
+++ глава 3/Project2/CodeFile1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+namespace Petzold.ShapeAnEllipse {
+    class ShapeAnEllipse : Window { [STAThread] // объявляется класс
+        public static void Main() // основная функция 
+        {
+            Application app = new Application(); // инициализация объекта
+            app.Run(new ShapeAnEllipse());
+        }
+        public ShapeAnEllipse()
+        {
+            Title = "Shape an Ellipse";
+            Ellipse elips = new Ellipse(); // инициализация эллипса
+            elips.Fill = Brushes.AliceBlue;
+            elips.StrokeThickness = 24; //1/4 inch - описание эллипса
+            elips.Stroke = new LinearGradientBrush(Colors.CadetBlue, Colors.Chocolate, new Point(1, 0), new Point(0, 1));
+            Content = elips;
+        }
+    }
+}
