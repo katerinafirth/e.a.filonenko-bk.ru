6788ef6cb1c767841ac6ee0d2fabf01ee1b72ead
katerinafirth <58289298+katerinafirth@users.noreply.github.com>
2020-04-05 15:07:08 +0500

    Add files via upload

---
+++ глава 20/глава 20/CodeFile1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+namespace Petzold.XamlCruncher
+{
+    public class XamlCruncherSettings : Petzold.NotepadClone.NotepadCloneSettings
+    {
+        public Dock Orientation = Dock.Left; // стандартные настройки предпочтения пользователя
+        public int TabSpaces = 4;
+        public string StartupDocument = "<Button xmlns=\"http://schemas .microsoft.com/winfx" + "/2006/xaml/presentation\" \r\n" + " xmlns:x=\"http://schemas .microsoft.com/winfx" + "/2006/xaml\">\r\n" + "Hello, XAML!\r\n" + "</Button>\r\n";
+
+
+        public XamlCruncherSettings() //конструктор для инициализации настроек в NotepadCloneSettings
+        {
+            string FontFamily = "Lucida Console";
+            double FontSize = 10 / 0.75;
+        }
+    }
+}
